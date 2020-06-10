d7fdbc7a1674da2f51fc4318f084bba0a99420b5
Dwight Doane <dwightdoane@me.com>
2020-06-10 01:14:11 -0400

    wip - setup building pages for gatsby

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,40 @@
  */
 
 // You can delete this file if you're not using it
+const path = require("path")
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode, basePath: `pages` })
+    createNodeField({ node, name: `slug`, value: slug })
+  }
+}
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const result = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    console.log({ node })
+    createPage({
+      path: node.fields.slug,
+      component: path.resolve(`./src/components/BlogPost.js`),
+      context: {
+        slug: node.fields.slug
+      }
+    })
+  })
+}
