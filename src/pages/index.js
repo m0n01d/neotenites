ca6fbdbfe7ccc92d8ef62bda21ba115e8c329b2f
Dwight Doane <dwightdoane@me.com>
2020-07-04 03:21:05 -0400

    wip - map stuff. got path started

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 import "./app.css"
-import { Map, TileLayer, Marker, Popup } from "react-leaflet"
+import { GeoJSON, Map, TileLayer, Marker, Popup } from "react-leaflet"
 import L from "leaflet"
 
 import Layout from "../components/layout"
@@ -65,15 +65,53 @@ const IndexPage = ({ data }) => {
 }
 
 const MyMap = () => {
+  const [a, b] = [28.7948813, -81.425958]
+  const [x, y] = [28.568864, -81.3566964]
+  const features = [
+    {
+      geometry,
+      type: "Feature",
+      properties: {
+        name: "dwit",
+        show_on_map: true
+      }
+    }
+  ]
+  const coordinates = [
+    [y, x],
+    [b, a]
+  ]
+
+  const geometryForLines = [
+    {
+      type: "LineString",
+      coordinates
+    }
+  ]
+  // Todo: add markers for each cooridinate
+  // Add date to coordinates so they can be use in marker
+  //
+  console.log({ features })
   return (
-    <Map
-      center={[51, -0.09]}
-      zoom={13}
-      style={{ width: "100%", height: "100vh" }}
-    >
+    <Map center={[a, b]} zoom={13} style={{ width: "100%", height: "100vh" }}>
       <TileLayer
         attribute="todo"
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <GeoJSON
+        style={i => ({
+          weight: 0.3,
+          //stroke-width: to have a constant width on the screen need to adapt with scale
+          opacity: 1,
+          dashArray: "3",
+          fillOpacity: 0.5,
+          fillColor: "red",
+          color: "#f00",
+          weight: 5,
+          opacity: 0.65
+        })}
+        data={geometry}
+        key={`test`}
       />
     </Map>
   )
